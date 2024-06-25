fc37df942f6f09f225e93743113b5823e2482db3
Eycom Gabriel <eycmgabrielgabriel@gmail.com>
2024-06-25 18:06:01 -0300

    first commit

---
+++ conversor.py
@@ -0,0 +1,9 @@
+class ConversorMedidas:
+    def __init__(self):
+        pass
+
+    def cm_to_m(cm):
+     return cm / 100
+
+    def m_to_cm(m):
+     return m * 100
---
+++ main.py
@@ -0,0 +1,25 @@
+from temperatura import ConversorTemperatura
+from conversor import ConversorMedidas
+
+
+print("1. converter temperatura celcius para fahrenheit\n2. converter temperatura fahrenheit para celcius\n3. converter medidas centimetros para metros\n4. converter medidas metros para centimetos")
+
+op = (input("Digite a opção desejada:"))
+    
+    
+if op == "1":
+        celsius = float(input("Digite a temperatura em Celsius: "))
+        fahrenheit = ConversorTemperatura.celsius_to_fahrenheit(celsius)
+        print(f"{celsius} graus Celsius é igual a {fahrenheit} graus Fahrenheit.")
+elif op == "2":
+        fahrenheit = float(input("Digite a temperatura em Fahrenheit: "))
+        celsius = ConversorTemperatura.fahrenheit_to_celsius(fahrenheit)
+        print(f"{fahrenheit} graus Fahrenheit é igual a {celsius} graus Celsius.")
+elif op == "3":
+      conversor = float(input("Digite a quantidade em centímetros que deseja converter para metros:\n")) / 100
+      print(f"O valor convertido é {conversor} metros.")
+elif op == "4":
+       conversor = float(input("Digite a quantidade em metros que deseja converter para centímetros:\n")) * 100
+       print(f"O valor convertido é {conversor} centímetros.")
+else:
+        print("Escolha inválida.")
